de53c57657a0075b2c3c56b8c9938da997edb2a3
Febri Eka Setyawan <thelazt16@users.noreply.github.com>
2019-06-22 15:17:41 +0700

    fix bspwm quit option

--- .scripts/powermenu.sh
+++ .scripts/powermenu.sh
@@ -3,7 +3,7 @@
 MENU="$(rofi -sep "|" -dmenu -i -p 'System' -width 35 -hide-scrollbar -line-padding 4 -padding 25 -lines 1 -columns 4 <<< " Lock| Logout| Reboot| Shutdown")"
             case "$MENU" in
                 *Lock) betterlockscreen -l ;;
-                *Logout) bspwm quit;;
+                *Logout) bspc quit;;
                 *Reboot) systemctl reboot ;;
                 *Shutdown) systemctl -i poweroff
             esac
