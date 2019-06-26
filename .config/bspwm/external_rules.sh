79ea439812747907a8dce8a4d1361880e01aef63
Febri Eka Setyawan <thelazt16@gmail.com>
2019-06-26 13:25:10 +0700

    Compton kawase to main, modify external_rules

--- .config/bspwm/external_rules.sh
+++ .config/bspwm/external_rules.sh
@@ -4,17 +4,30 @@ window_id=$1
 class=$2
 instance=$3
 
-#WM_WINDOW_ROLE Rules
-role=$(xprop -id $1 | grep WM_WINDOW_ROLE | awk -F '"' '{print $2}')
-case "$role" in
-  quake) #Tilix Quake
-    echo "state=floating"
-    echo "border=off"
-    ;;
-  pop-up|bubble|task_dialog|Preferences|dialog|menu)
-    echo "state=floating"
-    ;;
-  About|Organizer) #Firefox
-    echo "state=floating"
-    ;;
-esac
+#Class
+#notify-send 'Class:'"$class"
+if [[ $class == "Tilix" ]]; then
+  role=$(xprop -id $1 | grep WM_WINDOW_ROLE | awk -F '"' '{print $2}')
+  case $role in
+    quake) #Tilix Quake
+      echo "state=floating"
+      echo "border=off"
+      ;;
+  esac
+elif [[ $class == "Firefox" ]]; then
+  echo "split_ratio=0.35"
+  role=$(xprop -id $1 | grep WM_WINDOW_ROLE | awk -F '"' '{print $2}')
+  case $role in
+    About|Organizer) #Firefox About and Organizer(i.e. Downloads, Bookmarks)
+      echo "state=floating"
+      ;;
+  esac
+elif [[ $class == "smplayer" ]]; then
+  title=$(xprop -id $1 | grep WM_NAME | awk -F '"' '{print $2}')
+  case $title in
+    *playlist) # SMPlayer playlist
+      echo "state=tiled"
+      echo "split_ratio=0.82"
+      ;;
+  esac
+fi
