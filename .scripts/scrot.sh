56219cbcad222811651fcdab328c65b81672e63d
Febri Eka Setyawan <thelazt16@users.noreply.github.com>
2021-04-02 11:31:37 +0700

    Delete .scripts directory

--- .scripts/scrot.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-scrot '%Y-%m-%d_%H:%M:%S_dimmed_scr.png' -q 85  -e 'mv $f ~/Pictures/Screenshots/'
-notify-send --urgency=low --hint=string:x-dunst-stack-tag:scrot 'Screenshot Taken'
