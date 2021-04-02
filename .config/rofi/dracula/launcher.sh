01961e1499aa0a4b781588794003e5b29295f154
Febri Eka Setyawan <thelazt16@gmail.com>
2021-04-02 13:27:43 +0700

    Adding rofi config

---
+++ .config/rofi/dracula/launcher.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+FILE="$HOME/.config/rofi/dracula/config/colors.rasi"
+
+rofi -no-config -no-lazy-grab -show drun -modi drun -theme $HOME/.config/rofi/dracula/config/launcher.rasi
